b4a8b571261ef248691363b938131ce37993055f
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-03-24 22:20:41 -0300

    Update userController.js

--- Livraria/src/controllers/userController.js
+++ Livraria/src/controllers/userController.js
@@ -6,4 +6,16 @@ routes.get("/", (request, response) => {
     response.status(200).send("Deu tudo certo!")
 })
 
+routes.post("/", (request, response) => {
+    response.status(200).send("Método Post ok!")
+
+    const name = request.body.name;
+    const email = request.body.email;
+    const password = request.body.password;
+
+    console.log(name)
+    console.log(email)
+    console.log(password)
+})
+
 export default routes;
