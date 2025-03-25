573cdd68d31adb7f9bfcb63dcfd13b2a342a6798
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-03-24 22:20:06 -0300

    Update index.js

--- Livraria/src/index.js
+++ Livraria/src/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import routes from "./routes.js"
 
 const server = express();
+server.use(express.json());
 
 server.use("/", routes);
 
 server.listen(3333, () => {
-    console.log("Servidor está ok ");
+    console.log("Ta indo ne");
 });
-
