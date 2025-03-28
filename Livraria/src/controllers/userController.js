5818fb13ea49663522a4b16eecbb60bbbd411e3d
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-03-27 22:22:25 -0300

    Adding validation methods

--- Livraria/src/controllers/userController.js
+++ Livraria/src/controllers/userController.js
@@ -1,3 +1,4 @@
+import e from "express";
 import express, { request, response } from "express";
 
 const routes = express.Router();
@@ -7,17 +8,38 @@ routes.get("/", (request, response) => {
 })
 
 routes.post("/", (request, response) => {
-    response.status(200).send("Método Post ok!")
 
     //const name = request.body.name;
     //const email = request.body.email;
     //const password = request.body.password;
-    
-    const{name, email, password} = request.body;
-    
-    console.log(name)
-    console.log(email)
-    console.log(password)
+
+    const{name, email, password, typeUser} = request.body;
+
+    //console.log(name)
+    //console.log(email)
+    //console.log(password)
+
+    //Validação dos campos obrigatórios
+    if(name.length < 1) {
+        return response.status(400).send({"response": "Campo 'name' necessita de um caractere."});
+    }
+
+    //validação e-mail
+    if (!email.includes("@")) {
+        return response.status(400).send({ "response": "O campo 'email' está em um formato inválido." });
+    }
+
+    //validação de tamanho de senha 
+    if(password.length < 6) {
+        return response.status(400).send({"response": "A senha ter que ter no minimo 6 caracteres"})
+    }
+
+    //Validação tipo de usuario
+    if(typeUser !== "admin" && typeUser !== "comum") {
+        return response.status(400).send({"response": "Você tem que ser 'admin' ou 'comum'"})
+    }
+
+    return response.status(201).send({"message": "Cadastrado com sucesso", "user": {name, email, typeUser}})
 })
 
 export default routes;
