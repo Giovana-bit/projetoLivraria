fe71b3b146693d6e83406957095b56340760da8b
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-04-03 22:17:07 -0300

    Update user.js

--- Livraria/src/entities/user.js
+++ Livraria/src/entities/user.js
@@ -5,7 +5,13 @@ const user = new EntitySchema({
     tableName:"user",
     columns: {
         id: {primary: true, type: "int", generated: "increment"},
-        name: {type: "varchar", length: 50, nullable: false}
+        name: {type: "varchar", length: 50, nullable: false},
+        password: {type: "varchar", length: 50, nullable: false},
+        email: {type: "varchar", length: 50, nullable: false},
+        typeUser: {type: "enum", enum: ["admin", "comum"], nullable: false},
+        createAt: {type: "datetime", nullable: false, default: () =>
+            "CURRENT_TIMESTAMP"},
+        deleteAt: {type: "datetime", nullable: true}   
     }
 });
 
