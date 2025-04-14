d1b85c2e6bd7c80a2eec3fa5edc81948f29e59ad
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-04-14 19:10:36 -0300

    Delete Livraria directory

--- Livraria/src/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import userController from "./controllers/userController.js";
-
-const route = express();
-
-route.use("/user", userController);
-
-export default route;
