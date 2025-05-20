2d912344a5530c17251ad973620fb5dd9a506acc
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-05-19 22:31:29 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import userController from "./controllers/userController.js";
+import authorController from "./controllers/authorControler.js";
+import category from "./controllers/category.js";
+import publisherController from "./controllers/publisherController.js";
+
 
 const route = express();
 
 route.use("/user", userController);
+route.use("/author", authorController);
+route.use("/category", category);
+route.use("/publisher", publisherController);
 
 export default route;
