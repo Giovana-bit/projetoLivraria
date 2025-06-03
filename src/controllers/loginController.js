ac27f2538fb2b7ca7e21d2faff822bfc59374223
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-06-02 22:10:41 -0300

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,6 +2,7 @@ import express, { request, response } from "express";
 import User from "../entities/user.js";
 import { AppDataSource } from "../database/data-source.js";
 import { Like, IsNull } from "typeorm";
+import {generateToken} from "../utils/jwt.js";
 
 const routes = express.Router();
 const userRepository = AppDataSource.getRepository(User);
@@ -21,7 +22,15 @@ routes.post("/", async (request, response) => {
         email, password, deleteAt:IsNull()
     });
 
-    console.log (">>>>>>", user);
+    if (!user) {
+        return response.status(401).send({"response":"Usuário ou senha incorreta"});
+    }
+    const token = generateToken({user:user.name, email:user.email, typeUser:user.typeUser})
+    return response.status(200).send({
+     response: "Login efetuado com sucesso!",
+     token
+    });
+
 });
 
 export default routes;
