fa4403a25846f3e4b08745649a57b74010a62c2b
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-06-09 22:14:00 -0300

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,36 +1,68 @@
-import express, { request, response } from "express";
+import express from "express";
 import User from "../entities/user.js";
 import { AppDataSource } from "../database/data-source.js";
-import { Like, IsNull } from "typeorm";
-import {generateToken} from "../utils/jwt.js";
+import { IsNull } from "typeorm";
+import { generateToken } from "../utils/jwt.js";
+import { generateNewPassword } from "../utils/login.js";
+import { sendEmail } from "../helpers/nodemailer.js";
 
 const routes = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 
+// Rota de login
 routes.post("/", async (request, response) => {
-    const {email, password} = request.body;
+    const { email, password } = request.body;
 
     if (!email.includes("@")) {
-        return response.status(400).send({"response":"O email informado é invalido"});
+        return response.status(400).send({ response: "O email informado é inválido!" });
     }
 
-    if (password.length < 6){
-        return response.status(400).send({"response":"A senha deve conter pelo menos 6 caracteres"});
+    if (password.length < 6) {
+        return response.status(400).send({ response: "A senha deve conter pelo menos 6 caracteres." });
     }
 
     const user = await userRepository.findOneBy({
-        email, password, deleteAt:IsNull()
+        email,
+        password,
+        deleteAt: IsNull()
     });
 
     if (!user) {
-        return response.status(401).send({"response":"Usuário ou senha incorreta"});
+        return response.status(401).send({ response: "Usuário ou senha inválida" });
     }
-    const token = generateToken({user:user.name, email:user.email, typeUser:user.typeUser})
-    return response.status(200).send({
-     response: "Login efetuado com sucesso!",
-     token
+
+    const token = generateToken({
+        user: user.name,
+        email: user.email,
+        typeUser: user.typeUser
     });
 
+    return response.status(200).send({
+        response: "Login efetuado com sucesso!",
+        token
+    });
+});
+
+// Rota de redefinição de senha
+routes.put("/reset", async (request, response) => {
+    const { email } = request.body;
+
+    const user = await userRepository.findOneBy({
+        email,
+        deleteAt: IsNull()
+    });
+
+    if (!user) {
+        return response.status(400).send({ response: "Email inválido!" });
+    }
+
+    const newPassword = generateNewPassword();
+
+    await userRepository.update({ email }, { password: newPassword });
+
+    await sendEmail(newPassword, email);
+
+    return response.status(200).send({ response: "Senha enviada para o email cadastrado" });
 });
 
 export default routes;
