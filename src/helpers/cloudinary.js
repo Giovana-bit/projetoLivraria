c2c1bbf823d138ae0c7043f1c3e688eeeb91b8a0
Giovana Santos <120347155+Giovana-bit@users.noreply.github.com>
2025-06-17 21:55:11 -0300

    add new files

---
+++ src/helpers/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: "duzadjibm",
+    api_key: "489771465647254",
+    api_secret: "cBxKbnhqIN0opsfHnmcQLKZukfM"
+});
+
+export default cloudinary;
